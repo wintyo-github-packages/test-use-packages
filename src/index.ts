8f5d9e285cdac8f9c375d11a8833f0fe1d8b9233
wintyo <wintyo1024@gmail.com>
2023-04-08 12:17:05 +0900

    permissonなしでもGITHUB_TOKENでinstallできるか検証

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { add } from '@wintyo-github-packages/utils';
-import { hello } from '@wintyo-github-packages/private-utils';
+// import { hello } from '@wintyo-github-packages/private-utils';
 
 console.log(add(1, 2));
-hello('wintyo');
+// hello('wintyo');
